5a7a4d32c0589fac2c9a3f01c20e3a47fa05557a
WinstonYa <2215444192@qq.com>
2020-11-14 10:40:40 +0800

    feat: insert

---
+++ js/rem.js
@@ -0,0 +1,8 @@
+function resizeRem() {
+  let docElement = document.documentElement;
+  let width = docElement.getBoundingClientRect().width;
+  let rem = width / 10;
+  docElement.style.fontSize = rem + 'px';
+}
+window.addEventListener('resize', resizeRem);
+window.addEventListener('pageshow', resizeRem);
