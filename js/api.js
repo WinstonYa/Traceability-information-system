3aa40e9f2e8952c94f130824b5a4a502790b3ba0
WinstonYa <2215444192@qq.com>
2020-11-17 16:02:21 +0800

    feat: 2020/11/17

--- js/api.js
+++ js/api.js
@@ -1,10 +1,12 @@
 const data = {
-     // ip: "http://192.168.8.132:8766"
+    // ip: "http://192.168.8.132:8766"
     // ip: "http://192.168.2.6:8766"
     //服务器地址
-     ip: "http://192.168.8.166:8766",
-     ip_2: "http://192.168.8.166:8766"
+    ip: "http://192.168.8.166:8766",
 
+    // ip: "http://39.104.61.47:8766",
+    // ip_2: "http://39.104.61.47:8765/api/comSys",
+    // ip_3: "http://39.104.61.47:8765"
 };
 
 function doApi(sucFunc, errFunc, comFunc, url, type, params, contentType, async) {
@@ -12,7 +14,7 @@ function doApi(sucFunc, errFunc, comFunc, url, type, params, contentType, async)
         url: data.ip + url,
         data: params,
         type: type,
-        async: async ? async : true,
+        async: async ?async :true,
         contentType: contentType,
         // async: true,
         success: sucFunc,
